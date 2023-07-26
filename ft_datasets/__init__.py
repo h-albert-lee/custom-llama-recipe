26dcb88173ab4658b9b10ee5c1a3de604c1f3f7c
albertmade <albertmade@ncsoft.com>
2023-07-26 16:39:46 +0900

    initial commit

---
+++ ft_datasets/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from .grammar_dataset import get_dataset as get_grammar_dataset
+from .sft_dataset import InstructionDataset as get_sft_dataset
---
+++ utils/dataset_utils.py
@@ -0,0 +1,37 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+import torch
+
+from functools import partial
+
+from ft_datasets import (
+    get_sft_dataset,
+)
+from typing import Optional
+
+
+DATASET_PREPROC = {
+    "cot_dataset": partial(get_sft_dataset, max_words=250),
+}
+
+
+def get_preprocessed_dataset(
+    tokenizer, dataset_config, split: str = "train"
+) -> torch.utils.data.Dataset:
+    if not dataset_config.dataset in DATASET_PREPROC:
+        raise NotImplementedError(
+            f"{dataset_config.dataset} is not (yet) implemented")
+
+    def get_split():
+        return (
+            dataset_config.train_split
+            if split == "train"
+            else dataset_config.test_split
+        )
+
+    return DATASET_PREPROC[dataset_config.dataset](
+        dataset_config,
+        tokenizer,
+        get_split(),
+    )
